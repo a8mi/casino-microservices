e649736be3353cee9ae472770c4f930342ec7482
amina <aminaalsorani@gmail.com>
2026-08-09 13:29:15 +0200

    feat: Swagger/SpringDoc Dependency fuer Banking Service hinzugefuegt

--- roulette-service/src/test/java/roulette_service/RouletteServiceApplicationTests.java
+++ roulette-service/src/test/java/roulette_service/RouletteServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package roulette_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RouletteServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
